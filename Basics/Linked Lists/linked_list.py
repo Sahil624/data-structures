6ad4876a7e6cc44dd7b02b24751909a6ed794df8
Sahil Mangotra <sahilmangotra1997@gmail.com>
2018-01-01 22:10:36 +0530

    Added comments to function

--- Basics/Linked Lists/linked_list.py
+++ Basics/Linked Lists/linked_list.py
@@ -1,9 +1,11 @@
 from node import Node
 
 class Linked_list:
+	# Constructor
 	def __init__(self):
 		self.head = None
 
+	# Printing List
 	def print_list(self):
 		temp = self.head
 
@@ -11,11 +13,13 @@ class Linked_list:
 			print(temp.data)
 			temp = temp.next
 
+	# Inserting at Starting
 	def push(self,new_data):
 		new_node = Node(new_data)
 		new_node.next = self.head
 		self.head = new_node
 
+	# Inserting After Specific Node
 	def insert_after(self,prev,new_data):
 
 		if prev is None:
@@ -26,6 +30,7 @@ class Linked_list:
 		new_node.next = prev.next
 		prev.next = new_node
 
+	# Inserting At Last of Linked list
 	def append(self,new_data):
 		new_node = Node(new_data)
 
